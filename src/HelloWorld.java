658beb2f4a26c2bb88b7e5d72aee93dc8bef8d11
Matteo Greco <matteogrco9@gmail.com>
2018-06-15 13:03:37 +0200

    Upload Project

---
+++ src/HelloWorld.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import data.Data;
+import data.OutOfRangeSampleSize;
+import mining.KMeansMiner;
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+
+    /**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+    
+	throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        String table= request.getParameter("table");
+        int i= Integer.parseInt(request.getParameter("k"));
+        PrintWriter out = response.getWriter();
+        Data data = new Data(table);
+		KMeansMiner mining = new KMeansMiner(i);
+		try {
+			int numIt = mining.kmeans(data);
+		} catch (OutOfRangeSampleSize e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("ciaoooooo");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("Antonio gAY");
+//        out.println(data);
+//        out.println(mining.getC().toString(data));
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
---
+++ src/mining/Cluster.java
@@ -0,0 +1,80 @@
+package mining;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import data.*;
+@SuppressWarnings("serial")
+public class Cluster implements Serializable {
+	private Tuple centroid;
+
+	private Set<Integer> clusteredData; 
+	/*Cluster(){
+	
+	}*/
+
+	Cluster(Tuple centroid){
+		this.centroid=centroid;
+		clusteredData=new HashSet<Integer>();
+		
+	}
+		
+	Tuple getCentroid(){
+		return centroid;
+	}
+	
+	void computeCentroid(Data data){
+		for(int i=0;i<centroid.getLength();i++){
+			centroid.get(i).update(data,clusteredData);			
+		}
+		
+	}
+	//return true if the tuple is changing cluster
+	boolean addData(int id){
+		return clusteredData.add(id);
+		
+	}
+	
+	//verifica se una transazione è clusterizzata nell'array corrente
+	boolean contain(int id){
+		return clusteredData.contains(id);
+	}
+	
+
+	//remove the tuplethat has changed the cluster
+	void removeTuple(int id){
+		clusteredData.remove(id);
+		
+	}
+	
+	public String toString(){
+		System.out.println("aaa");
+		String str="Centroid=(";
+		for(int i=0;i<centroid.getLength();i++)
+			str+=centroid.get(i);
+		str+=")";
+		return str;
+		
+	}
+	
+
+	
+	public String toString(Data data){
+		String str="Centroid=(";
+		for(int i=0;i<centroid.getLength();i++)
+			str+=centroid.get(i)+ " ";
+		str+=")<BR>Examples:<BR>";
+		Object[] array=clusteredData.toArray();
+		for(int i=0;i<array.length;i++){
+			str+="[";
+			for(int j=0;j<data.getNumberOfAttributes();j++)
+				str+=data.getAttributeValue((int) array[i], j)+" ";
+			str+="] dist="+getCentroid().getDistance(data.getItemSet((int) array[i]))+"<BR>";
+			
+		}
+		str+="<BR>AvgDistance="+getCentroid().avgDistance(data, array);
+		return str;
+		
+	}
+
+}
