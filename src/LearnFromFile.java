143053372e63bc14f570596276ac33f3631bd3d4
Matteo Greco <matteogrco9@gmail.com>
2018-06-20 13:15:22 +0200

    capocchia

--- src/LearnFromFile.java
+++ src/LearnFromFile.java
@@ -2,13 +2,19 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import data.Data;
 import mining.KMeansMiner;
 
-public class LearnFromFile {
+public class LearnFromFile extends HttpServlet  {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException  {
 		
