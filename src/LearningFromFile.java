0b214686238d50c03c254178766b50bb830af4db
Matteo Greco <matteogrco9@gmail.com>
2018-06-20 13:08:17 +0200

    learningfromfile

---
+++ src/LearningFromFile.java
@@ -0,0 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import data.Data;
+import mining.KMeansMiner;
+
+public class LearningFromFile {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException  {
+		
+		String namefile=request.getParameter("name");
+		try {
+			Data data = new Data("playtennis");
+			KMeansMiner mining = new KMeansMiner(namefile);
+			request.setAttribute("mining",mining.getC().toString(data));
+			request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);			
+			
+			
+		}catch( FileNotFoundException | ClassNotFoundException e) {
+			request.setAttribute("mining","Erroor");
+			request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+}
